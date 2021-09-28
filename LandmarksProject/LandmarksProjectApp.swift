20fd47985952443cc994dd97bee61dac1b9715a8
selcuk baki <selcukbaki@windowslive.com>
2021-09-28 04:48:52 +0300

    Initial Commit

---
+++ LandmarksProject/LandmarksProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksProjectApp.swift
+//  LandmarksProject
+//
+//  Created by Selcuk Baki on 28/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
