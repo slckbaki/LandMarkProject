e1647d2d7a39a66c064e2aaf54640361e5ef0e7f
selcuk baki <selcukbaki@windowslive.com>
2021-10-04 00:59:02 +0300

    clear build

--- LandmarksProject/DetailsView.swift
+++ LandmarksProject/DetailsView.swift
@@ -9,42 +9,30 @@ import SwiftUI
 
 struct DetailsView: View {
     var body: some View {
-        NavigationView {
             VStack {
-                    
                 List(landmarkArray) { landmark in
                     HStack {
                         Image(landmark.image)
                             .resizable()
                             .frame(width: 100, height: 100, alignment: .center)
                             .scaledToFit()
-                            .clipShape(Circle())
-                            .padding(.trailing, 25)
                         VStack(alignment: .center) {
                             Text(landmark.name)
                                 .font(.title)
-                                .position(x: 90, y: 40)
+                                .position(x: 115, y: 40)
                             Text(landmark.country)
                             Text(landmark.category)
                         }
                     }
                 }
                 ZStack {
-
                     MapView(coordinate: landmarkArray[0].locationCoordinate)
                         .frame(height: UIScreen.main.bounds.height * 0.4)
-                    
                     CircleImageView(image: Image("london"))
                         .frame(width : UIScreen.main.bounds.width * 0.1, height : UIScreen.main.bounds.height * 0.1)
                         .position(x: UIScreen.main.bounds.width / 2  , y : 30)
-                        
                 }
-
-
-
-            }.navigationBarHidden(true)
-            
-        }
+            }
     }
 }
 
